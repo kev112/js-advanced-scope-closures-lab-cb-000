f4398c3d9f51b512ac3d827c5a886c26f7738215
Tao <werewaswas@live.com>
2018-07-15 04:07:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function produceDrivingRange(number) {
   return function (firstAve, secondAve) {
-    const toInt = (string) => {parseInt(string.slice(0,-2) )};
+    const toInt = function (string) {
+      parseInt(string.slice(0,-2) )
+    };
     firstAve = toInt(firstAve);
     secondAve = toInt(secondAve);
     distance = Math.abs(secondAve - firstAve);
 
     const outOfRange = (distance > 10) ? true : false
     if (outOfRange) {
-      return `${distance} blocks out of range`
+      return `${distance} blocks out of range`;
     } else {
-      return `within range by ${distance}`
+      return `within range by ${distance}`;
     }
 
   }
