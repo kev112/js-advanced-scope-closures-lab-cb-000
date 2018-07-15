0f47478ba332b1f1378c289b537498f4e128a497
Tao <werewaswas@live.com>
2018-07-15 04:00:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function produceDrivingRange(number) {
   return function (firstAve, secondAve) {
-    const toInt = (string) => {parseInt(string.splice(0,-2))};
+    const toInt = (string) => {parseInt(string.splice(0,-2) )};
     firstAve = splice(firstAve);
     secondAve = splice(secondAve);
-    
+
   }
 }
-
