e2bf287e9ada29f70857bc1783882229f79bb2e3
Tao <werewaswas@live.com>
2018-07-15 04:13:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function produceDrivingRange(number) {
+  function toInt (string) {
+    return parseInt(string.slice(0,-2) )
+  };
+  
   return function (firstAve, secondAve) {
-    function toInt (string) {
-      return parseInt(string.slice(0,-2) )
-    };
+
+    
     firstAve = toInt(firstAve);
     secondAve = toInt(secondAve);
     let distance = Math.abs(secondAve - firstAve);
